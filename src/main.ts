468264bf9813e2bdc0c5d281e81d99fa6a13a9a7
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-06-17 23:00:47 +0800

    feat: util functions, draw functions
    - 图形绘制函数

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,112 @@
 import Canvas2DApplication from './application/Canvas2DApplication';
 import { EnumColor } from './utils/Color';
+import Vec2 from './utils/Vec2';
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+interface RenderConfig {
+  fillStyle?: string | CanvasGradient | CanvasPattern;
+  strokeStyle?: string | CanvasGradient | CanvasPattern;
+  lineWidth?: number;
+  lineCap?: CanvasLineCap;
+  lineDash?: number[];
+  lineDashOffset?: number;
+}
+
+const decratorContext2D = (
+  context: CanvasRenderingContext2D,
+  renderConfig: RenderConfig
+) => {
+  const {
+    fillStyle,
+    strokeStyle,
+    lineWidth,
+    lineCap,
+    lineDash,
+    lineDashOffset,
+  } = renderConfig;
+  if(fillStyle)
+    context.fillStyle = fillStyle;
+  if(strokeStyle)
+    context.strokeStyle = strokeStyle;
+  if(lineWidth)
+    context.lineWidth = lineWidth;
+  if(lineCap)
+    context.lineCap = lineCap;
+  if(lineDash)
+    context.setLineDash(lineDash);
+  if(lineDashOffset)
+    context.lineDashOffset = lineDashOffset;
+}
+const actionContext2D = (
+  context: CanvasRenderingContext2D,
+  renderConfig: RenderConfig
+) => {
+  if(renderConfig.fillStyle) {
+    context.fill();
+  }
+  if(
+    renderConfig.strokeStyle ||
+    (!renderConfig.strokeStyle && !renderConfig.fillStyle)
+  ) {
+    context.stroke();
+  }
+}
 
 class Application extends Canvas2DApplication {
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
   }
 
-  drawRect(x: number, y: number,width:number, height: number, config: any = {}) {
+  drawRect(x: number, y: number,width:number, height: number, config: RenderConfig = {}) {
     const { context2D } = this;
-    if (!context2D) throw new Error('get content error...');
+    if (!context2D) throw new Error('get context error...');
     context2D.save();
-    context2D.fillStyle = config.fillStyle;
-    context2D.strokeStyle = config.strokeStyle || EnumColor.PURPLE;
-    context2D.lineWidth = config.lineWidth;
-    // context2D.rect(x, y, width, height);
+    decratorContext2D(context2D, config);
     context2D.beginPath();
     context2D.moveTo(x, y);
     context2D.lineTo(x + width, y);
     context2D.lineTo(x + width, y + height);
     context2D.lineTo(x, y + height);
     context2D.closePath();
-    context2D.stroke()
-    if (config.fillStyle)
-      context2D.fill();
+    actionContext2D(context2D, config);
+    context2D.restore();
+  }
+
+  drawCircle(
+    cx: number,
+    cy: number,
+    radius: number,
+    startAngle: number,
+    endAngle: number,
+    config: RenderConfig = {}
+  ) {
+    const { context2D } = this;
+    if (!context2D) throw new Error('get context error...');
+    context2D.save();
+    decratorContext2D(context2D, config);
+    context2D.beginPath();
+    context2D.arc(
+      cx,
+      cy,
+      radius,
+      startAngle,
+      endAngle
+    );
+    context2D.closePath();
+    actionContext2D(context2D, config);
+    context2D.restore();
+  }
+
+  drawLine(ptrBegin: Vec2, ptrEnd: Vec2, config: RenderConfig = {}) {
+    const { context2D } = this;
+    if (!context2D) throw new Error('get context error...');
+    decratorContext2D(context2D, config);
+    context2D.save();
+    context2D.beginPath();
+    context2D.moveTo(ptrBegin.x, ptrBegin.y);
+    context2D.lineTo(ptrEnd.x, ptrEnd.y);
+    context2D.closePath();
+    actionContext2D(context2D, config);
     context2D.restore();
   }
 
@@ -31,21 +114,27 @@ class Application extends Canvas2DApplication {
 
   update(elapsedMsec: number) {
     const { context2D } = this;
-    if (!context2D) throw new Error('get content error...');
+    if (!context2D) throw new Error('get context error...');
     context2D.clearRect(0, 0, canvas.width, canvas.height);
 
-    context2D.setLineDash([10, 5]);
-    context2D.lineDashOffset = this.lineDashOffset;
+    // context2D.strokeStyle = 'red'
     this.lineDashOffset = (this.lineDashOffset - 1) % 1000;
-    this.drawRect(100, 100, 100, 200, { lineWidth: 2 });
-
-    // context2D.fillStyle = 'orange';
-    // let speed = 10;
-    // const t = elapsedMsec / 1000;
-
-    // this.drawRect(t * speed,10, 100, 20);
-    // this.drawRect(t * speed, t * speed, 40, 20, { fillStyle: 'green' });
-    // this.drawRect(10, t * speed, 60, 30);
+    this.drawRect(100, 100, 100, 200, {
+      lineWidth: 2,
+      lineDash: [10, 5],
+      lineDashOffset: (this.lineDashOffset - 1) % 1000,
+      // strokeStyle: EnumColor.PURPLE
+    });
+    this.drawCircle(400, 400, 20, 0, Math.PI * 2, {
+      // strokeStyle: 'orange'
+    });
+    this.drawLine(
+      new Vec2(342, 200),
+      new Vec2(544, 333),
+      {
+        // strokeStyle: 'orange'
+      }
+    );
   }
 
   moveX(x: number, y: number, speed: number, borderX: number) {
@@ -60,7 +149,6 @@ class Application extends Canvas2DApplication {
 
 }
 
-
 const init = (canvas: HTMLCanvasElement) => {
   const clienWidth = document.documentElement.clientWidth;
   const clientHeight = document.documentElement.clientHeight;
@@ -69,8 +157,6 @@ const init = (canvas: HTMLCanvasElement) => {
 }
 
 init(canvas);
-
-
 
 const app = new Application(canvas);
 app.start()
