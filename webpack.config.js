2080ba025549270a4cbcf168be90c849ea18bc09
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-06-30 11:55:53 +0800

    ci(webpack.config.js): 配置webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devServer = {
+  contentBase: path.resolve(__dirname, 'out'),
+  port: 8000,
+  host: '127.0.0.1',
+  overlay: {
+    errors: true,
+  }
+}
 
 var config = {
   mode: 'development',
-  // devServer: {
-  //   port: 8000,
-  //   host: '127.0.0.1',
-  //   overlay: {
-  //       errors: true,
-  //   }
-  // },
+  devServer,
   entry: path.resolve(__dirname, './src/main.ts'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './out'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html',
+    })
+  ],
   resolve: {
     extensions: ['.ts', '.js', '.json', '.wasm'],
   },
